60ed9a10414181d23ca7f5276d685588acb93057
BubblenautIrene <bubblenautirene@gmail.com>
2026-01-26 21:13:01 +0100

    Adding functionality and tests e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :ExperimentCourse11="ExperimentCourse11" />
+</template>
+
+<script>
+import Vue from 'vue'
+import ExperimentCourse11 from '@ExperimentCourse11/core'
+import { Dashboard } from '@ExperimentCourse11/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    ExperimentCourse11: () => new ExperimentCourse11(),
+  },
+}
+</script>
+
+<style src="@ExperimentCourse11/core/dist/style.css"></style>
+<style src="@ExperimentCourse11/dashboard/dist/style.css"></style>
